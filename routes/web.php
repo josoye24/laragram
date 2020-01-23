9a3a9bef0450841e5d646751a732620ac2fa5c20
josoye24@gmail.com <josoye24@gmail.com>
2020-01-23 07:13:47 +0100

    argon theme added

--- routes/web.php
+++ routes/web.php
@@ -29,9 +29,18 @@ Route::get('/profile/{user}', 'ProfilesController@index')->name('profile.show');
 Route::get('/profile/{user}/edit', 'ProfilesController@edit')->name('profile.edit');
 Route::patch('/profile/{user}/edit', 'ProfilesController@update')->name('profile.update');
 
-Route::get('/', 'PostsController@index');
+//Route::get('/', 'PostsController@index');
 Route::get('/p/create', 'PostsController@create');
 Route::post('/p/create', 'PostsController@store');
 Route::get('/p/{post}', 'PostsController@show');
 
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::group(['middleware' => 'auth'], function () {
+	Route::resource('user', 'UserController', ['except' => ['show']]);
+	Route::get('profile', ['as' => 'profile.edit', 'uses' => 'ProfileController@edit']);
+	Route::put('profile', ['as' => 'profile.update', 'uses' => 'ProfileController@update']);
+	Route::put('profile/password', ['as' => 'profile.password', 'uses' => 'ProfileController@password']);
+});
 
